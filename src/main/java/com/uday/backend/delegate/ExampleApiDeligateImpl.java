4c4cc2afa04a9f3fdaa6afe17caa9e5ea687e784
Uday Kiran Tentu <87901946+uktentu@users.noreply.github.com>
2025-05-09 20:57:03 +0530

    Health check update and example (#4)

---
+++ src/main/java/com/uday/backend/delegate/ExampleApiDeligateImpl.java
@@ -0,0 +1,29 @@
+package com.uday.backend.delegate;
+
+import com.uday.backend.api.ExampleApiDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.uday.backend.model.ExampleResponse;
+
+@Service
+public class ExampleApiDeligateImpl implements ExampleApiDelegate {
+
+    private static final Logger log = LoggerFactory.getLogger(ExampleApiDeligateImpl.class);
+
+    @Override
+    public ResponseEntity<ExampleResponse> getExample() {
+        log.info("Received request for example data");
+
+        // Create a sample response
+        ExampleResponse response = new ExampleResponse();
+        response.setId("1");
+        response.setName("Example Name");
+        response.setDescription("This is an example description.");
+
+        log.debug("Response created: {}", response);
+        // Return the response with HTTP 200 status
+        return ResponseEntity.ok(response);
+    }
+}
